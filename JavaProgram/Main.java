c39118322381af7c7c0cfcd6e4cc50e7dc3bfe1b
rohityadu257 <rohityadu257@gmail.com>
2022-01-29 23:03:31 +0530

    my

---
+++ JavaProgram/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main (String[] args)
+	{
+		int num1 = 7;
+	
+		for (int i=1; i<11; i++)
+		{	
+		System.out.println(num1 + "x" + (i) +" ="+  num1*(i) );
+	}
+}
+} 
